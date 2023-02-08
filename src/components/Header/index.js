8e7bdd8070072ee22cc582b5a2332a0e24e41aff
escowin <edwin@escowinart.com>
2023-02-07 22:40:05 -0600

    set up header & fa icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMobile, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 
-function Header({vishions}) {
-    const navigation = [
-        "product1",
-        "product2",
-        "product3",
-        "product4"
-    ]
+function Header({ vishions }) {
+  const navigation = ["link1", "link2", "link3", "link4"];
 
   return (
     <header>
       <aside className="logo">{vishions.name} logo</aside>
+
+      <section className="contact">
+        <p>
+          {vishions.address}
+          <FontAwesomeIcon className="icon" icon={faLocationDot} />
+          </p>
+        <p>
+          {vishions.contact.phone}
+          <FontAwesomeIcon className="icon" icon={faMobile} />
+        </p>
+      </section>
+
       <nav>
-        <ul className="navigation">
-            {navigation.map((link, i) => (
-                <li key={i} className="link">{link}</li>
-            ))}
+        <ul className="menu">
+          {navigation.map((link, i) => (
+            <li key={i} className="link">
+              {link}
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
