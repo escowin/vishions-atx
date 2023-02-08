2afaefe183287bb1fbf40f33bc14770aebcac862
escowin <edwin@escowinart.com>
2023-02-07 20:42:15 -0600

    passed down object prop from app to header, mapped navigation array

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import Header from './components/Header'
 import Homepage from './components/Homepage'
 
 function App() {
+  const vishions = {
+    name: "Vishions ATX",
+    address: "3207 Red River Street, Austin TX",
+    hours: ["Mon-Sat : 10a-10p", "Sun: 10a-9p"],
+    contact: {
+      phone: "512-645-0575",
+      email: "vishions.atx@gmail.com",
+      ig: "vishions-atx",
+    },
+  };
+
   return (
     <div className="body">
-      <Header/>
+      <Header vishions={vishions}/>
       <main>
         main
         <Homepage/>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,25 @@
-import './index.css';
+import "./index.css";
 
-function Header() {
-    const vishions = {
-            name: "Vishions ATX",
-            address: "3207 Red River Street, Austin TX",
-            hours: ["Mon-Sat : 10a-10p", "Sun: 10a-9p"],
-            contact: {
-                phone: "512-645-0575",
-                email: "vishions.atx@gmail.com",
-                ig: "vishions-atx"
-            }
-        }
+function Header({vishions}) {
+    const navigation = [
+        "product1",
+        "product2",
+        "product3",
+        "product4"
+    ]
 
-    return (
-        <header>
-            <h1>{vishions.name}</h1>
-            <nav>
-                <ul>
-                    <li></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <aside className="logo">{vishions.name} logo</aside>
+      <nav>
+        <ul className="navigation">
+            {navigation.map((link, i) => (
+                <li key={i} className="link">{link}</li>
+            ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
